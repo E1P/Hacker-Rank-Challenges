f9562598c8def9266ccc1c6c269417b79564539f
E1P <ewan.pearce@gmail.com>
2019-05-03 19:27:43 +0100

    Set up files for new year chaos

--- __tests__/left-rotation.test.js
+++ __tests__/left-rotation.test.js
@@ -1,15 +1,19 @@
 const { leftRotation } = require("../arrays/left-rotation");
 const testArray = [1, 2, 3, 4, 5];
 const oneRotArray = [2, 3, 4, 5, 1];
+const fourRotArray = [5, 1, 2, 3, 4];
 
 describe("leftRotation()", () => {
   it("is a function", () => {
     expect(typeof leftRotation).toBe("function");
   });
   it("returns same array if rotations = 0", () => {
-    expect(leftRotation(0, testArray)).toEqual(testArray);
+    expect(leftRotation(testArray, 0)).toEqual(testArray);
   });
   it("rotates by one", () => {
-    expect(leftRotation(1, testArray)).toEqual(oneRotArray);
+    expect(leftRotation(testArray, 1)).toEqual(oneRotArray);
+  });
+  it("rotates by four", () => {
+    expect(leftRotation(testArray, 4)).toEqual(fourRotArray);
   });
 });
---
+++ __tests__/new-year-chaos.test.js
@@ -0,0 +1,7 @@
+const { newYearChaos } = require("../arrays/new-year-chaos");
+
+describe("NewYearChaos()", () => {
+  it("should be a function", () => {
+    expect(typeof newYearChaos).toBe("function");
+  });
+});
