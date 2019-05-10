d71d990e70bb5f186862d560498b7b74a207424b
E1P <ewan.pearce@gmail.com>
2019-05-10 16:56:22 +0100

    Add function and tests for arrayManipulation. Needs to be more efficient.

--- __tests__/array-manipulation.test.js
+++ __tests__/array-manipulation.test.js
@@ -1,7 +1,25 @@
-const { arrayManipulation } = require("../arrays/array-manipulation");
+const { processArrayQueries, arrayManipulation } = require("../arrays/array-manipulation");
 
-describe("arrayManipulation", () => {
+describe("processArrayQueries", () => {
   it("is a function", () => {
-    expect(typeof arrayManipulation).toBe("function");
+    expect(typeof processArrayQueries).toBe("function");
+  });
+  it("builds an array of zeroes with length n and returns it when no queries are given", () => {
+    expect(processArrayQueries(5, null)).toEqual([0, 0, 0, 0, 0]);
+  });
+  it("adds number k to each index in single query", () => {
+    expect(processArrayQueries(5, [[1, 3, 1]])).toEqual([1, 1, 1, 0, 0]);
+  });
+  it("adds number k to each index from two queries", () => {
+    expect(processArrayQueries(5, [[1, 3, 1], [3, 4, 1]])).toEqual([1, 1, 2, 1, 0]);
+  });
+});
+
+describe("arrayManipulation()", () => {
+  it("returns largest value after single-query manipulation", () => {
+    expect(arrayManipulation(5, [[1, 3, 1]])).toBe(1);
+  });
+  it("returns largest value after double-query manipulation", () => {
+    expect(arrayManipulation(5, [[1, 3, 1], [3, 4, 1]])).toBe(2);
   });
 });
--- arrays/array-manipulation.js
+++ arrays/array-manipulation.js
@@ -1,3 +1,53 @@
-exports.arrayManipulation = input => {
-  return input;
+const processArrayQueries = (n, queries) => {
+  const zeroArray = [];
+  for (let i = 0; i < n; i++) {
+    zeroArray[i] = 0;
+  }
+  if (queries) {
+    return queries.reduce((output, query) => {
+      const aIndex = query[0] - 1;
+      const bIndex = query[1];
+      const k = query[2];
+      for (let i = aIndex; i < bIndex; i++) {
+        output[i] += k;
+      }
+      return output;
+    }, zeroArray);
+  } else {
+    return zeroArray;
+  }
 };
+
+const arrayManipulation = (n, queries) => {
+  const zeroArray = [];
+  let maximum = 0;
+  for (let i = 0; i < queries.length; i++) {
+    let a = queries[i][0] - 1;
+    let b = queries[i][1];
+    let k = queries[i][2];
+    for (let j = a; j < b; j++) {
+      zeroArray[j] = (zeroArray[j] || 0) + k;
+      console.log(zeroArray);
+      if (zeroArray[j] > maximum) maximum = zeroArray[j];
+    }
+  }
+  return maximum;
+  // const zeroArray = [];
+  // for (let i = 0; i < n; i++) {
+  //   zeroArray[i] = 0;
+  // }
+  // let maximum = 0;
+  // queries.reduce((output, query) => {
+  //   const aIndex = query[0] - 1;
+  //   const bIndex = query[1];
+  //   const k = query[2];
+  //   for (let i = aIndex; i < bIndex; i++) {
+  //     output[i] += k;
+  //     if (output[i] > maximum) maximum = output[i];
+  //   }
+  //   return output;
+  // }, zeroArray);
+  // return maximum;
+};
+
+module.exports = { processArrayQueries, arrayManipulation };
