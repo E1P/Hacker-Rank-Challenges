2ebef5492506f947b21f1fead670990f89c50c6a
E1P <ewan.pearce@gmail.com>
2019-05-10 14:56:32 +0100

    Set up files for array-manipulation challenge

---
+++ __tests__/array-manipulation.test.js
@@ -0,0 +1,7 @@
+const { arrayManipulation } = require("../arrays/array-manipulation");
+
+describe("arrayManipulation", () => {
+  it("is a function", () => {
+    expect(typeof arrayManipulation).toBe("function");
+  });
+});
---
+++ arrays/array-manipulation.js
@@ -0,0 +1,3 @@
+exports.arrayManipulation = input => {
+  return input;
+};
