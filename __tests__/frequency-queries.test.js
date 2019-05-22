5942d40438af2a336376e737375ca5072a5240fa
E1P <ewan.pearce@gmail.com>
2019-05-22 13:04:35 +0100

    Add working functions and tests. Needs to be more performant.

--- __tests__/frequency-queries.test.js
+++ __tests__/frequency-queries.test.js
@@ -1,7 +1,25 @@
-const { frequencyQueries } = require("../hash-tables/frequency-queries");
+const { frequencyQueries, createRef } = require("../hash-tables/frequency-queries");
+
+describe("createRef()", () => {
+  it("returns an object which tallies according to given queries", () => {
+    expect(createRef([[1, 1], [1, 2], [1, 1]])).toEqual({ 1: 2, 2: 1 });
+  });
+  it("deletes item if query number is 2", () => {
+    expect(createRef([[1, 1], [1, 2], [1, 1], [2, 1]])).toEqual({ 1: 1, 2: 1 });
+  });
+  it("only deletes item if it exists", () => {
+    expect(createRef([[1, 1], [1, 2], [1, 1], [2, 3]])).toEqual({ 1: 2, 2: 1 });
+  });
+});
 
 describe("frequencyQueries()", () => {
   it("Is a function", () => {
     expect(typeof frequencyQueries).toBe("function");
+  });
+  it("returns an array with one 0 when query contains single 3 without applicable frequency", () => {
+    expect(frequencyQueries([[1, 1], [1, 2], [1, 1], [2, 1], [3, 2]])).toEqual([0]);
+  });
+  it("returns an array with one 1 when query contains single 3 with applicable frequency", () => {
+    expect(frequencyQueries([[1, 1], [1, 2], [1, 1], [3, 2]])).toEqual([1]);
   });
 });
--- hash-tables/frequency-queries.js
+++ hash-tables/frequency-queries.js
@@ -1,5 +1,30 @@
-const frequencyQueries = () => {
-  return "i'm a function";
+const createRef = array => {
+  return array.reduce((ref, query) => {
+    if (query[0] === 1) {
+      ref[query[1]] = (ref[query[1]] || 0) + 1;
+    }
+    if (query[0] === 2 && ref[query[1]]) {
+      ref[query[1]]--;
+    }
+    return ref;
+  }, {});
 };
 
-module.exports = { frequencyQueries };
+const frequencyQueries = array => {
+  const output = [];
+  array.reduce((ref, query) => {
+    if (query[0] === 1) {
+      ref[query[1]] = (ref[query[1]] || 0) + 1;
+    }
+    if (query[0] === 2 && ref[query[1]]) {
+      ref[query[1]]--;
+    }
+    if (query[0] === 3) {
+      output.push(Object.values(ref).includes(query[1]) ? 1 : 0);
+    }
+    return ref;
+  }, {});
+  return output;
+};
+
+module.exports = { frequencyQueries, createRef };
